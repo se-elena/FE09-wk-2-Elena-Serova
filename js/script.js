25277995d57115655600bc6215c1a0d9c45fa366
Serova Elena <e.serova1706@gmail.com>
2021-04-03 16:56:18 +0300

    project 2

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  $('.slider-photo').slick({
+    arrows: false,
+    dots: false,
+    adaptiveHeight: true,
+    slidesToShow:1,
+    slidesToScroll: 1,
+    speed: 7000,
+    easing: 'ease',
+    infinfte: true,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 800,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    pauseOnDotsHover: true,
+    draggable: true,
+    swipe: true,
+    touchThreshold: 5,
+    touchMove: true,
+    waitForAnimate: false,
+    centerMode: false,
+    variableWidth: false,
+    rows: 1,
+    slidesPerRow: 1,
+    vertical: false,
+    fade: true,
+    asNavFor:".slider-text"
+  });
+  $('.slider-text').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    speed: 1000,
+    asNavFor:".slider-photo"
+  });
+});
